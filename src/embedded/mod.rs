eba9e2a933c7320269f537ec228de5d21d5dd337
John Shaffer <jdsha@proton.me>
2022-07-14 16:54:48 -0500

    Get remove-reviewed step working

--- src/embedded/mod.rs
+++ src/embedded/mod.rs
@@ -1,15 +1,19 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
+use std::io::BufRead;
 use std::io::BufReader;
 use std::path::PathBuf;
 
 use serde::Deserialize;
+use serde::Serialize;
 
 pub mod remove_reviewed;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Config {
     db: Option<String>,
+    reviewer: Option<String>,
 }
 
 pub struct Env {
@@ -18,16 +22,23 @@ pub struct Env {
     output: Option<PathBuf>,
 }
 
-pub fn get_config(
-    filename: PathBuf,
-) -> Result<Config, Box<dyn std::error::Error>> {
+#[derive(Deserialize, Serialize)]
+pub struct Event {
+    data: Option<serde_json::Value>,
+    #[serde(flatten)]
+    extra: HashMap<String, serde_json::Value>,
+    hash: Option<String>,
+    r#type: String,
+}
+
+pub fn get_config(filename: PathBuf) -> Result<Config, Box<dyn std::error::Error>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
     let result = serde_json::from_reader(reader).or_else(|err| Err(Box::new(err)));
 
     // Force conversion to the correct return type
     // https://stackoverflow.com/a/57426673
-    return result.or_else(|err| Err(Box::new(err) as Box<dyn std::error::Error>));
+    result.or_else(|err| Err(Box::new(err) as Box<dyn std::error::Error>))
 }
 
 pub fn get_env() -> Result<Env, Box<dyn std::error::Error>> {
@@ -40,4 +51,18 @@ pub fn get_env() -> Result<Env, Box<dyn std::error::Error>> {
         input,
         output,
     })
+}
+
+pub fn parse_event(s: &str) -> Result<Event, Box<dyn std::error::Error>> {
+    let result = serde_json::from_str(s).or_else(|err| Err(Box::new(err)));
+
+    // Force conversion to the correct return type
+    // https://stackoverflow.com/a/57426673
+    result.or_else(|err| Err(Box::new(err) as Box<dyn std::error::Error>))
+}
+
+pub fn events(
+    reader: BufReader<File>,
+) -> impl Iterator<Item = Result<Event, Box<dyn std::error::Error>>> {
+    reader.lines().map(|line| parse_event(line?.as_str()))
 }
