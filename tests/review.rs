d22d29b5566280d4dd7405879fd479e06813057c
John Shaffer <john@insilica.co>
2022-08-19 15:43:18 -0500

    Add test for wrong review flow name

--- tests/review.rs
+++ tests/review.rs
@@ -1,9 +1,14 @@
+use std::path::PathBuf;
+
 mod common;
 
 #[test]
 fn test_simple() -> Result<(), std::io::Error> {
     let dir = "test-resources/simple";
-    common::delete(dir, "sink.jsonl");
+    let sink = PathBuf::from(dir).join("sink.jsonl");
+    if sink.exists() {
+        std::fs::remove_file(&sink)?;
+    };
     common::cmd(200)
         .current_dir(dir)
         .args(&["review", "simple"])
@@ -15,5 +20,23 @@ fn test_simple() -> Result<(), std::io::Error> {
         (0, String::from("")),
         common::file_diff(dir, "sink.jsonl", "expected.jsonl")?
     );
+    Ok(())
+}
+
+#[test]
+fn test_wrong_name() -> Result<(), std::io::Error> {
+    let dir = "test-resources/wrong-name";
+    let sink = PathBuf::from(dir).join("sink.jsonl");
+    if sink.exists() {
+        std::fs::remove_file(&sink)?;
+    };
+    common::cmd(100)
+        .current_dir(dir)
+        .args(&["review", "simpel"])
+        .assert()
+        .code(1)
+        .stdout("")
+        .stderr("Error: No flow named \"simpel\" in \"sr.yaml\"\n");
+    assert_eq!(false, sink.exists());
     Ok(())
 }
