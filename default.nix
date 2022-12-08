23b1e1e0ec01f6b4ce5d285269c31a339281c12e
John Shaffer <john@insilica.co>
2022-12-08 12:36:15 -0600

    Add Security framework for mac builds

--- default.nix
+++ default.nix
@@ -1,3 +1,10 @@
 { sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
-with pkgs;
-mkShell { buildInputs = [ cargo git niv nixfmt rustc rustfmt webfs ]; }
+let target = pkgs.stdenv.targetPlatform;
+in with pkgs;
+mkShell {
+  buildInputs = [ cargo git niv nixfmt rustc rustfmt webfs ]
+    ++ (if target.isDarwin then
+      [ darwin.apple_sdk.frameworks.Security ]
+    else
+      [ ]);
+}
