1850076ef6ad0b0293b91d0a2563431341e227f3
John Shaffer <john@insilica.co>
2022-12-16 17:36:42 -0600

    Remove old nix compat files
    No one is using these, they are just clutter.

--- default.nix
+++
@@ -1,6 +0,0 @@
-# flake-compat makes Nix flakes compatible with the old Nix cli commands, like nix-build and nix-shell.
-(import (fetchTarball {
-  url =
-    "https://github.com/edolstra/flake-compat/archive/009399224d5e398d03b22badca40a37ac85412a1.tar.gz";
-  sha256 = "sha256:0xcr9fibnapa12ywzcnlf54wrmbqqb96fmmv8043zhsycws7bpqy";
-}) { src = ./.; }).defaultNix
