67a1132e84833efbc33ba19f690cee26b7e4c7d9
John Shaffer <john@insilica.co>
2022-12-30 16:24:26 -0600

    Add libiconv for macOS builds

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 let target = pkgs.stdenv.targetPlatform;
 in with pkgs;
 mkShell {
-  buildInputs = [ cargo git niv nixfmt rustc rustfmt webfs ]
+  buildInputs = [ cargo git libiconv niv nixfmt rustc rustfmt webfs ]
     ++ (if target.isDarwin then
       [ darwin.apple_sdk.frameworks.Security ]
     else
