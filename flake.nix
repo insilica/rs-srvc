19633d6d5714af847de2b04c88b4f295a06e44bf
John Shaffer <jdsha@proton.me>
2023-02-01 12:53:27 -0600

    Use flake-compat for nix-shell
    nix develop is still marked experimental and is not enabled everywhere,
    so we want to use nix-shell as the default (in .envrc) for now.
    The previous shell.nix worked but did not pin the nixpkgs version.
    This way does.

--- flake.nix
+++ flake.nix
@@ -4,24 +4,44 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        self-rev = if (self ? rev) then self.rev else null;
         srvc = pkgs.callPackage ./srvc.nix {
           Security = with pkgs;
             lib.optionals stdenv.isDarwin
             [ darwin.apple_sdk.frameworks.Security ];
-            self-rev = if (self ? rev) then self.rev else null;
+          self-rev = self-rev;
         };
       in with pkgs; {
         packages.default = srvc;
         packages = { inherit srvc; };
-        devShells.default = import ./shell.nix {
-          inherit pkgs;
-          self-rev = if (self ? rev) then self.rev else null;
+        devShells.default = mkShell {
+          buildInputs = [
+            cargo
+            entr
+            git
+            libiconv
+            nixfmt
+            rustc
+            rustfmt
+            sphinx
+            sphinx-autobuild
+          ] ++ (with python3Packages; [ sphinx-rtd-theme ])
+            ++ (if stdenv.isDarwin then
+              [ darwin.apple_sdk.frameworks.Security ]
+            else
+              [ ]);
+
+          SELF_REV = self-rev;
         };
       });
 
--- shell.nix
+++ shell.nix
@@ -1,13 +1,6 @@
-{ pkgs ? import <nixpkgs> { }, self-rev ? null }:
-let target = pkgs.stdenv.targetPlatform;
-in with pkgs;
-mkShell {
-  buildInputs =
-    [ cargo entr git libiconv nixfmt rustc rustfmt sphinx sphinx-autobuild ]
-    ++ (with python3Packages; [ sphinx-rtd-theme ]) ++ (if target.isDarwin then
-      [ darwin.apple_sdk.frameworks.Security ]
-    else
-      [ ]);
-
-  SELF_REV = self-rev;
-}
+(import (let lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+in fetchTarball {
+  url =
+    "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+  sha256 = lock.nodes.flake-compat.locked.narHash;
+}) { src = ./.; }).shellNix
