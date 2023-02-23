a3e5fc244a721f1e75f1c39fe8328eb1b114916a
John Shaffer <jdsha@proton.me>
2023-02-23 17:27:34 -0600

    Add constraints to ensure that label-answer targets exist

--- src/lib/schema/sqlite.sql
+++ src/lib/schema/sqlite.sql
@@ -26,4 +26,26 @@ CREATE INDEX IF NOT EXISTS idx_srvc_event_label_answer_reviewer
   ON srvc_event (data->>'$.reviewer')
   WHERE type = 'label-answer';
 
+CREATE TRIGGER IF NOT EXISTS srvc_event_label_answer_document_constraint
+AFTER INSERT ON srvc_event
+WHEN NEW.type = 'label-answer'
+BEGIN
+  SELECT RAISE(ABORT, 'Missing document event for label-answer')
+  WHERE NOT EXISTS (
+    SELECT 1 FROM srvc_event
+    WHERE hash = NEW.data->>'$.document' AND type = 'document'
+  );
+END;
+
+CREATE TRIGGER IF NOT EXISTS srvc_event_label_answer_label_constraint
+AFTER INSERT ON srvc_event
+WHEN NEW.type = 'label-answer'
+BEGIN
+  SELECT RAISE(ABORT, 'Missing label event for label-answer')
+  WHERE NOT EXISTS (
+    SELECT 1 FROM srvc_event
+    WHERE hash = NEW.data->>'$.label' AND type = 'label'
+  );
+END;
+
 COMMIT;
