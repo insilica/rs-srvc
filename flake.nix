5c40852ab03810eeafe4b7bd21518255439c1aea
John Shaffer <john@insilica.co>
2024-08-13 08:13:30 -0500

    Upgrade deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "SysRev Version Control";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -26,7 +26,7 @@
             entr
             git
             libiconv
-            nixfmt
+            nixfmt-classic
             rustc
             rustfmt
             sphinx
